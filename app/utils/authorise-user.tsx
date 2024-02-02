044064c9e39fe9213ac89208b31361260adf3525
thomasrichmond <thomas.richmond8@gmail.com>
2024-02-02 23:04:30 +1100

    server action to authorise user and callback

---
+++ app/utils/authorise-user.tsx
@@ -0,0 +1,15 @@
+// "use client";
+import axios from "axios";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default function RequestLogin() {
+  const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+  const redirectURI = "http://localhost:3000/api/callback";
+  const url = "https://accounts.spotify.com/authorize";
+  const scope =
+    "ugc-image-upload user-modify-playback-state user-read-playback-state user-read-recently-played user-top-read app-remote-control playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative user-follow-modify user-follow-read user-library-modify user-library-read user-read-email user-read-private";
+  const params = `response_type=code&client_id=${clientId}&scope=${scope}&redirect_uri=${redirectURI}`;
+
+  redirect(`${url}?${params}`);
+}
