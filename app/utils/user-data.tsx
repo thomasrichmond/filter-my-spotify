b13f96603d9e6dfbe32c22a16f610e9e02c9cd83
thomasrichmond <thomas.richmond8@gmail.com>
2024-02-04 23:43:24 +1100

    Add total song count func to load all savedSongs

--- app/utils/user-data.tsx
+++ app/utils/user-data.tsx
@@ -1,8 +1,29 @@
 import axios from "axios";
 import { cookies } from "next/headers";
 
+// * Below are server side util functions to call data in the app
+// * these are designed to be used in SSR components or used as functions in CSR comps
+
 const cookie = cookies();
 const authToken = cookie.get("t")?.value;
+
+export async function getTotalSavedSongs() {
+  const totalSongs = await axios
+    .get(`https://api.spotify.com/v1/me/tracks`, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    })
+    .then((res) => {
+      return res.data.total;
+    })
+    .catch((err) => {
+      console.log(":::ERROR:::", err?.response?.data);
+      return err?.response?.data?.error?.status;
+    });
+
+  return totalSongs;
+}
 
 export async function getSavedSongs(nextPageOfResults?: number) {
   const indexOffset = nextPageOfResults ?? 0;
