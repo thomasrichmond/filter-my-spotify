2bf0a2dba1fc374a6a3cb02bd81e3496b58e529a
thomasrichmond <thomas.richmond8@gmail.com>
2024-02-05 23:53:42 +1100

    Total song render in user component

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,17 +1,22 @@
 import axios from "axios";
 import { IUserProps } from "./User.types";
 import {
+  getAllSavedSongs,
   getSavedSongs,
   getTopSongs,
+  getTotalSavedSongs,
   getUserInformation,
 } from "@/app/utils/user-data";
 import SavedSongs from "../SavedSongs";
 import { Suspense } from "react";
+import TotalSongs from "../TotalSongs";
 
 const User = async ({}: IUserProps) => {
   const accountDetails = await getUserInformation();
   const savedSongs = await getSavedSongs();
   const topSongs = await getTopSongs();
+
+  const totalSongList = await getAllSavedSongs();
 
   // TODO Build in retry token functionality when data returns 401
   // TODO this needs to happen to reduce undefined val errors with data fetching
@@ -29,6 +34,9 @@ const User = async ({}: IUserProps) => {
       </h3>
       <Suspense fallback={<h1>Loading...</h1>}>
         <SavedSongs songPayload={savedSongs} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading total songs...</h1>}>
+        <TotalSongs />
       </Suspense>
     </div>
   );
