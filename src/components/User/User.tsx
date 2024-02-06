c2c0dfa06bab8bd2dad3d74f4cdb80989fa6eca1
thomasrichmond <thomas.richmond8@gmail.com>
2024-02-06 22:23:25 +1100

    build user logic for token expiry and recapture

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -10,34 +10,48 @@ import {
 import SavedSongs from "../SavedSongs";
 import { Suspense } from "react";
 import TotalSongs from "../TotalSongs";
+import { getRefreshToken } from "@/app/utils/authorise-user";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 const User = async ({}: IUserProps) => {
-  const accountDetails = await getUserInformation();
-  const savedSongs = await getSavedSongs();
-  const topSongs = await getTopSongs();
+  let accountDetails = await getUserInformation();
+  let savedSongs = await getSavedSongs();
+  let topSongs = await getTopSongs();
+  let requestSuccess = true;
 
-  const totalSongList = await getAllSavedSongs();
+  // const totalSongList = await getAllSavedSongs();
 
-  // TODO Build in retry token functionality when data returns 401
-  // TODO this needs to happen to reduce undefined val errors with data fetching
+  //* Refresh token logic so user can stay signed into app
   if (savedSongs === 401 || topSongs === 401) {
-    //retryToken();
-    //Get song data again
+    requestSuccess = false;
+    const refresh = await getRefreshToken();
+
+    if (refresh) {
+      requestSuccess = true;
+      // revalidatePath("/");
+      // TODO confirm if the redirect triggers the re-redner correctly
+      redirect("/");
+    }
   }
 
   return (
     <div>
-      <h1>Hello {accountDetails?.display_name}</h1>
-      <h3>
-        Here are all of {accountDetails?.display_name}&apos;s recently saved
-        songs
-      </h3>
-      <Suspense fallback={<h1>Loading...</h1>}>
-        <SavedSongs songPayload={savedSongs} />
-      </Suspense>
-      <Suspense fallback={<h1>Loading total songs...</h1>}>
-        <TotalSongs />
-      </Suspense>
+      {requestSuccess && (
+        <>
+          <h1>Hello {accountDetails?.display_name}</h1>
+          <h3>
+            Here are all of {accountDetails?.display_name}&apos;s recently saved
+            songs
+          </h3>
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <SavedSongs songPayload={savedSongs} />
+          </Suspense>
+          <Suspense fallback={<h1>Loading total songs...</h1>}>
+            <TotalSongs />
+          </Suspense>
+        </>
+      )}
     </div>
   );
 };
