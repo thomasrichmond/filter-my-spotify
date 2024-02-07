02532487cb853ba7b0dfefd89760525bfdd13a61
thomasrichmond <thomas.richmond8@gmail.com>
2024-02-07 23:30:51 +1100

    Add refresh token route & remove its server action

--- app/api/refresh/route.ts
+++ app/api/refresh/route.ts
@@ -1,38 +1,44 @@
 import axios from 'axios';
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
-import { getAccessToken, setAccessToken } from '../_auth';
 
 export async function GET(request: NextRequest) {
-
+  const refreshToken = request.nextUrl.searchParams.get('r') ?? null
+  const url = "https://accounts.spotify.com/api/token";
+  let refreshSuccess = false;
   const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
   const clientSecret = process.env.NEXT_PUBLIC_CLIENT_SECRET;
-  const credentialsBase64 = Buffer.from(`${clientId}:${clientSecret}`).toString('base64');
-  const refreshToken = getAccessToken()?.refreshToken
-  const url = 'https://accounts.spotify.com/api/token'
+  const credentialsBase64 = Buffer.from(`${clientId}:${clientSecret}`).toString(
+    "base64"
+  );
+  let access = '';
 
 
-  const postBody = {
-    'grant_type': 'refresh_token',
-    'refresh_token': refreshToken
-  }
+  const body = {
+    grant_type: "refresh_token",
+    refresh_token: refreshToken,
+    client_id: clientId,
+  };
 
-  const postHeaders = {
-    'content-type': 'application/x-www-form-urlencoded',
-    'Authorization': `Basic ${credentialsBase64}`
-  }
+  await axios
+    .post(url, body, {
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${credentialsBase64}`,
+      },
+    })
+    .then((res: any) => {
+      refreshSuccess = true;
+      console.log(res.data)
+      access = res.data.access_token
+    })
+    .catch((err) => {
+      console.log(err?.response?.data);
+    });
 
-  await axios.post(url, postBody, { headers: postHeaders }).then((res) => {
-    const accessObject = {
-      token: res.data.access_token,
-      tokenType: res.data.token_type,
-      expiresIn: res.data.expires_in,
-      refreshToken: res.data.refresh_token,
-      scope: res.data.scope
-    }
+  let response = NextResponse.json(refreshSuccess, { status: 200 })
+  response.cookies.set("cook", access!, { httpOnly: true });
+  response.cookies.set("t", access!, { httpOnly: true });
 
-    setAccessToken(accessObject)
-  })
-
-  return NextResponse.json({ status: 200 })
+  return response
 }
--- app/utils/authorise-user.tsx
+++ app/utils/authorise-user.tsx
@@ -1,14 +1,15 @@
-// "use client";
+"use server";
 import axios from "axios";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { getAccessToken } from "../api/_auth";
+import { access } from "fs";
 
 const cookie = cookies();
 const authToken = cookie.get("t")?.value;
 const refreshToken = cookie.get("r")?.value;
 
-export default function RequestLogin() {
+export default async function RequestLogin() {
   const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
   const redirectURI = "http://localhost:3000/api/callback";
   const url = "https://accounts.spotify.com/authorize";
@@ -17,37 +18,4 @@ export default function RequestLogin() {
   const params = `response_type=code&client_id=${clientId}&scope=${scope}&redirect_uri=${redirectURI}`;
 
   redirect(`${url}?${params}`);
-}
-
-export async function getRefreshToken() {
-  const url = "https://accounts.spotify.com/api/token";
-  let refreshSuccess = false;
-  const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
-  const clientSecret = process.env.NEXT_PUBLIC_CLIENT_SECRET;
-  const credentialsBase64 = Buffer.from(`${clientId}:${clientSecret}`).toString(
-    "base64"
-  );
-  const body = {
-    grant_type: "refresh_token",
-    refresh_token: refreshToken,
-    client_id: clientId,
-  };
-
-  await axios
-    .post(url, body, {
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-        Authorization: `Basic ${credentialsBase64}`,
-      },
-    })
-    .then((res: any) => {
-      refreshSuccess = true;
-      cookies().set("t", res.data.access_token, { httpOnly: true });
-      cookies().set("r", res.data.refresh_token, { httpOnly: true });
-    })
-    .catch((err) => {
-      console.log(err?.response?.data);
-    });
-
-  return refreshSuccess;
 }
