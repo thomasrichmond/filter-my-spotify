7ead655a5d48ece0ee29980f36a881aeda734fc0
thomasrichmond <thomas.richmond8@gmail.com>
2024-02-18 23:18:24 +1100

    adding genre data to card prop in Song component

--- src/components/SavedSongs/SavedSongs.tsx
+++ src/components/SavedSongs/SavedSongs.tsx
@@ -24,7 +24,7 @@ const SavedSongs = ({
       .get(`api/saved-songs?skip=${skipIndex}`)
       .then((res) => {
         let resArray: any = [];
-        res.data.items.forEach((song: any, index: number) => {
+        JSON.parse(res.data).forEach((song: any, index: number) => {
           resArray.push(song);
         });
         setUserData(userData.concat(resArray));
@@ -50,6 +50,7 @@ const SavedSongs = ({
         title={song.track.name}
         description={song.track.artists[0].name}
         imageUrl={song.track.album.images[0].url}
+        genre={song.genre}
       />
     );
   });
