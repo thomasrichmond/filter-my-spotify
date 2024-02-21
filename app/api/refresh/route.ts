900ff6b37d690a7f78eec88ad09217acc0ae85e7
thomasrichmond <thomas.richmond8@gmail.com>
2024-02-21 22:37:55 +1100

    removing unused logs

--- app/api/refresh/route.ts
+++ app/api/refresh/route.ts
@@ -31,7 +31,6 @@ export async function GET(request: NextRequest) {
     })
     .then((res: any) => {
       refreshSuccess = true;
-      console.log(res.data)
       access = res.data.access_token
     })
     .catch((err) => {
